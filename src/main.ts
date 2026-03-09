8353956343e9bf63c0006322811c538b0fcdca7f
UnunNeeranuch <neeranuch.k42@gmail.com>
2026-03-09 17:23:06 +0700

    feat: apply global pipe and global guards

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { GlobalValidationPipe } from './common/pipes/global-valiation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new GlobalValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
